6a715c55fc63e9d4310923e7f5c1a709c592d8a6
Leonschiffeles <leon.dchiffeles@gmail.com>
2023-06-06 21:13:37 +0200

    add js funktion  change Html

---
+++ Index.js
@@ -0,0 +1,5 @@
+function next() {
+    document.getElementById("hthema").classList.remove("hide");
+    document.getElementById("wbutt").classList.add("hide");
+    document.getElementById("dthema").classList.remove("hide");
+}
