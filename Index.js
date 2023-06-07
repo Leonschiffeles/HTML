fbab7556357c9971c58e1289513d51f278b6e29b
Leonschiffeles <leon.dchiffeles@gmail.com>
2023-06-07 10:06:20 +0200

    change js and create a new page in spil. htm

--- Index.js
+++ Index.js
@@ -3,3 +3,39 @@ function next() {
     document.getElementById("wbutt").classList.add("hide");
     document.getElementById("dthema").classList.remove("hide");
 }
+
+function Probiren() {
+    let result = 0;
+
+    let a1 = document.getElementById("f1").value;
+    if (a1 === "Marienplatz") {
+        result++;
+    }
+
+    let a2 = document.getElementById("f2").value;
+    if (a2 === "Olympiaturm") {
+        result++;
+    }
+
+    let a3 = document.getElementById("f3").value;
+    if (a3 === "Statue der Bavaria") {
+        result++;
+    }
+
+    let a4 = document.getElementById("f4").value;
+    if (a4 === "Frauenkirche") {
+        result++;
+    }
+
+    let a5 = document.getElementById("f5").value;
+    if (a5 === "Englischer Garten") {
+        result++;
+    }
+
+    let a6 = document.getElementById("f6").value;
+    if (a6 === "Schloss Nymphenburg") {
+        result++;
+    }
+
+    alert("Richtige Antworten..." + result);
+}
